6d51ea34034e16c9df98ab13a38144f6b3e1eb46
Krishna Hendra <krisnahendrawijaya@gmail.com>
2020-07-15 16:43:26 +0700

    first

---
+++ application/controllers/Admin.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __CONSTRUCT(){
+		parent::__construct();
+		$this->load->model('M_Admin','admin');
+	}
+
+	public function index()
+	{
+		$data['konten']= 'admin/dashboard';
+		$this->load->view('admin/template',$data);
+	}
+
+	public function film(){
+		$data['film']= $this->admin->show_film();
+		$data['konten']= 'admin/film';
+		$this->load->view('admin/template',$data);
+	}
+
+	public function add_film(){
+		$this->admin->add_film();
+	}
+
+	public function user(){
+		$data['konten']= 'admin/data_user';
+		$this->load->view('admin/template',$data);
+	}
+
+	public function delete_film($id_film){
+        $this->admin->delete_film($id_film);
+    }
+}
---
+++ application/views/admin/data_user.php
@@ -0,0 +1,1 @@
+<h2>DATA USER</h2>
